b85145cf8f2a127fdc17e42208056b76e17105a1
RoyEden <roy.ede@gmail.com>
2020-07-15 03:47:09 -0300

    Listado de temas!

--- src/pages/temas.js
+++ src/pages/temas.js
@@ -1,10 +1,59 @@
 import React from "react"
 import Image from "gatsby-image"
+import styled from "@emotion/styled"
 import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import TransitionLink from "../components/transitionLink"
+import astCompiler from "../utils/astCompiler"
+
+const Container = styled.div`
+  min-width: 360px;
+  width: 80%
+`
+
+const Link = styled(TransitionLink)`
+  width: 100%;
+`
+
+const Article = styled.article`
+  align-items: center;
+  background-color: ${({ theme }) => theme.background};
+  color: ${({ theme }) => theme.color};
+  display: flex;
+  height: 150px;
+  justify-content: space-around;
+  overflow: hidden;
+  padding: 0 0 0 2rem;
+  @media (max-width: 768px) {
+    padding: 0 2rem;
+  }
+  @media (max-width: 554px) {
+    padding: 0 1rem;
+  }
+`
+
+const ArticleNumber = styled.p`
+  flex: 1;
+`
+
+const ArticleTitle = styled.header`
+  flex: 3;
+  padding: 0 8px;
+`
+
+const ArticleDescription = styled.p`
+  flex: 3;
+  padding: 0 8px;
+`
+
+const ArticleImage = styled.div`
+  flex: 1;
+  margin-left: 16px;
+  min-height: 60px;
+  min-width: 60px;
+`;
 
 const Temas = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
@@ -13,56 +62,43 @@ const Temas = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="Lista de temas" />
-      {posts.map(({ node }) => {
-        const titulo = node.frontmatter.titulo || node.fields.slug
-        const {
-          imagen,
-          color_transicion,
-          direccion_transicion,
-          duracion_transicion,
-          tapar_transicion,
-          transicion,
-        } = node.frontmatter
-        return (
-          <article key={node.fields.slug}>
-            <TransitionLink
-              color_transicion={color_transicion}
+      <Container>
+        {posts.map(({ node }, index) => {
+          const titulo = node.frontmatter.titulo || node.fields.slug
+          const {
+            descripcion,
+            imagen,
+            color_transicion,
+            direccion_transicion,
+            duracion_transicion,
+            tapar_transicion,
+            transicion,
+          } = node.frontmatter
+          return (
+            <Link
+              color_transicion={
+                transicion !== "cubrir" ? color_transicion : "#000"
+              }
               direccion_transicion={direccion_transicion}
               duracion_transicion={duracion_transicion}
               tapar_transicion={tapar_transicion}
               transicion={transicion}
               to={node.fields.slug}
             >
-              <header>
-                <h3
-                  style={{
-                    marginBottom: "0.25rem",
-                  }}
-                >
-                  {titulo}
-                </h3>
-              </header>
-              <section>
-                {imagen && (
-                  <div
-                    style={{
-                      height: 300,
-                      width: 300,
-                    }}
-                  >
-                    <Image fluid={imagen.childImageSharp.fluid} />
-                  </div>
-                )}
-                <p
-                  dangerouslySetInnerHTML={{
-                    __html: node.frontmatter.descripcion || node.excerpt,
-                  }}
-                />
-              </section>
-            </TransitionLink>
-          </article>
-        )
-      })}
+              <Article key={node.fields.slug}>
+                <ArticleNumber>{index + 1}</ArticleNumber>
+                <ArticleTitle>{titulo}</ArticleTitle>
+                <ArticleDescription>
+                  {descripcion ? descripcion : astCompiler(node.excerptAst)}
+                </ArticleDescription>
+                <ArticleImage>
+                  {imagen && <Image fluid={imagen.childImageSharp.fluid} />}
+                </ArticleImage>
+              </Article>
+            </Link>
+          )
+        })}
+      </Container>
     </Layout>
   )
 }
@@ -79,12 +115,13 @@ export const pageQuery = graphql`
     allMarkdownRemark(sort: { fields: [frontmatter___orden], order: ASC }) {
       edges {
         node {
-          excerpt
+          excerptAst
           fields {
             slug
           }
           frontmatter {
             titulo
+            descripcion
             color_transicion
             direccion_transicion
             duracion_transicion
@@ -92,10 +129,8 @@ export const pageQuery = graphql`
             transicion
             imagen {
               childImageSharp {
-                fluid(maxWidth: 360) {
+                fluid(maxHeight: 150, maxWidth: 150) {
                   ...GatsbyImageSharpFluid_tracedSVG
-                  presentationHeight
-                  presentationWidth
                 }
               }
             }
